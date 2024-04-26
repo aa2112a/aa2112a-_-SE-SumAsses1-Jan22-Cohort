13cc555c483423c3a6ebf3249af5f5a392b87715
aa2112a <aa2112a@students.nulondon.ac.uk>
2024-04-26 18:53:23 +0100

    added extreme tests. fixed processing issues which arouse with russian numbers starting with a  7

--- script.test.js
+++ script.test.js
@@ -7,11 +7,13 @@ describe('Test Standardise Msisdns', () => {
     expect(() => StandardiseMSISDNs(447473676677)).toThrowError('input is not a string');
     expect(StandardiseMSISDNs('0007473676677')).toEqual('+447473676677');
     expect(StandardiseMSISDNs('007473676677')).toEqual('+447473676677');
-    expect(StandardiseMSISDNs('07473676677')).toEqual('+447473676677');
+    expect(StandardiseMSISDNs('(0)7473676677')).toEqual('+447473676677');
     expect(StandardiseMSISDNs('447473676677')).toEqual('+447473676677');
     expect(StandardiseMSISDNs('7473676677')).toEqual('+447473676677'); 
     expect(StandardiseMSISDNs('31412345678')).toEqual('+31412345678'); 
-    expect(StandardiseMSISDNs('123')).toEqual('123 is not a valid msisdn (Reason: short length)')
-    expect(StandardiseMSISDNs('123456789012345')).toEqual('123456789012345 is not a valid msisdn (Reason: long length)')
+    expect(StandardiseMSISDNs('00074959567200')).toEqual('+74959567200'); 
+    expect(StandardiseMSISDNs('+7 495 956-72-00')).toEqual('+74959567200'); 
+    expect(StandardiseMSISDNs('123')).toEqual('123 is not a valid msisdn (Reason: short length)');
+    expect(StandardiseMSISDNs('123456789012345')).toEqual('123456789012345 is not a valid msisdn (Reason: long length)');
   })
 });
